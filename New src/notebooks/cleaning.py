12c48c1f7cb5c70e60a050359b6e18b54aee6b8d
unknown <sanasadat.mirahsani.etu@univ-lille.fr>
2025-05-13 11:21:01 +0200

    finish actor and binom and joker

--- New src/notebooks/cleaning.py
+++ New src/notebooks/cleaning.py
@@ -6,9 +6,9 @@
 #                  Library
 #------------------------------------------------
 import pandas as pd 
-import os
 from pandas import to_datetime, to_timedelta
 import re
+from typing import Optional
 
 #------------------------------------------------
 #                  Functions
@@ -16,6 +16,17 @@ import re
 
 # Change time format
 def clean_time(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Consider type of 'session.id' as str.
+    Change the format of 'timestamp.$date', 'time_delta', 'session.duration'.
+    Reset the indices.
+
+    Args:
+        df : A dataframe.
+
+    Returns:
+        Same dataframe but cleaned.
+    """
 
     # 1. Change the type of session.id to str
     if 'session.id' in df.columns: # check if the column exist
@@ -33,7 +44,7 @@ def clean_time(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 # Clean actor column
-def clean_actor(names) -> None:
+def clean_actor(names: str) -> Optional[str]:
     """
     clean actors with this pattern 'prenom.nom.etu'.
     This function doesn't get a dataframe, instead it is applied on a dataframe and 
@@ -41,10 +52,10 @@ def clean_actor(names) -> None:
     it changes them in-place without returning anything.
 
     Args:
-        name : A row of DataFrame.
+        name : Row by row with the str type of dataframe.
 
     Returns:
-        the cleaned name of actor
+        None or a cleaned name of actor as string.
     """
      
     #print(name) this is for test
@@ -57,37 +68,32 @@ def clean_actor(names) -> None:
         # 2. Remove ' '
         if '' in names : names.remove('')
 
-        #print(name) this is for test
+        #print(names) #this is for test
         
         # 3. Check pattern prenom.nom.etu for both students
+        cleaned = [] # it only takes one or two values (prenom.nom.etu)
         for name in names:
             if re.match(pattern, name):
-                return name  # valid, keep as is
+                cleaned.append(name)  # valid, keep as is
             
             elif '@' in name:  # maybe correctable
                 match = re.match(r'^([a-zA-Z\-]+\.[a-zA-Z0-9\-]+\.etu)@', name)
 
                 if match:
-                    return match.group(1)  # extract clean part
-            else:
-                #ToDO
-                pass
-     # discard bad names
+                    cleaned.append(match.group(1)) # extract clean part
 
-# Seperate actor column
-def split_actor_column(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Seperate actors name and put the binom's name into the binom column.
+            else: # Jokers, must change
+                # Remove nebut and luc
+                if name == 'nebut' or name == 'luc':
+                    cleaned.append(None)
+                    print(name)
 
-    Args:
-        name : DataFrame.
+                elif name == 'MI1301' or name == 'MI1304':
+                    cleaned.append('mariama-sere.sylla.etu')
 
-    Returns:
-        Same dataframe with the column binom.
-    """
-
-    df = df.copy()
-    df['binom'] = df['actor'].apply(lambda x: x[1] if len(x) > 1 else None) # Add column binom
-    df['actor'] = df['actor'].apply(lambda x: x[0])
-
-    return df
+        # Return first name as 'actor', second as 'binom' (if exists)
+        if cleaned:
+            return pd.Series([cleaned[0], cleaned[1] if len(cleaned) > 1 else None])
+        
+    # if the name is not str type
+    return pd.Series([None, None])
