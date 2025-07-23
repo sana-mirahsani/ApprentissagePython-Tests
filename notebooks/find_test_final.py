90f9f3e08a78729e020d6e8431d10edf1fc4f640
Mirabelle Marvie Nebut <mirabelle.nebut@univ-lille.fr>
2025-07-23 17:43:48 +0200

    version finale revue recherche des tests tp2 à 9

--- notebooks/find_test_final.py
+++ notebooks/find_test_final.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.17.1
+#       jupytext_version: 1.17.2
 #   kernelspec:
 #     display_name: venv_jupyter_l1test
 #     language: python
@@ -28,7 +28,7 @@ import numpy as np
 import difflib
 
 # just for test
-from src.data.variable_constant_2425 import SORTED_SEANCE, all_TP_functions_name, all_TP_prog_functions_name_by_tp
+from src.data.variable_constant_2425 import SORTED_SEANCE
 import re
 from src.data.variable_constant_2425 import FILES_BY_TP, TP_NAME
 from src.features import data_cleaning
@@ -123,7 +123,7 @@ FUNCTIONS_TP6_Prog = [
     "sans_elt",
     "positive",
     "chiffres",
-    "miroir",
+    #"miroir", à enlever car présent ds 2 TP
     "compte_car"
 ]
 
@@ -133,7 +133,7 @@ FUNCTIONS_TP6_Prog = [
 FUNCTIONS_TP7_Prog = [
     "echantillonne",
     "elements_indices_impairs",
-    "miroir",
+    #"miroir", à enlever car présent ds 2 TP
     "minimum",
     "decoupage",
     "premieres_occurrences",
@@ -194,7 +194,19 @@ FUNCTIONS_TP9_Prog = [
     "genere_hexa_sans_begaiement",
 
 ]
+# -
 
+
+PROG_FUNCTIONS_NAME_BY_TP = {
+    'Tp2' : FUNCTIONS_TP2_Prog,
+    'Tp3' : FUNCTIONS_TP3_Prog,
+    'Tp4' : FUNCTIONS_TP4_Prog,
+    'Tp5' : FUNCTIONS_TP5_Prog,
+    'Tp6' : FUNCTIONS_TP6_Prog,
+    'Tp7' : FUNCTIONS_TP7_Prog,
+    'Tp8' : FUNCTIONS_TP8_Prog,
+    'Tp9' : FUNCTIONS_TP9_Prog,
+}
 
 # +
 from thonnycontrib.backend.test_finder import L1TestFinder
@@ -202,10 +214,13 @@ from thonnycontrib.exceptions import SpaceMissingAfterPromptException
 
 def find_tests_in_codestate(source:str) -> dict:
     '''
-    Renvoie un dictionnaire qui associe à chaque fonction trouvée dans `source` son nombre de tests.
-    Renvoie None si le source n'est pas analysable car SyntaxError ou erreur de syntaxe dans les tests
+    Returns a dictionnary which associates to each function found in `source` its number of tests (using l1test), or None if `source` cannot be parsed either by Python or the l1test parser.
     
-    source is some codestate
+    Args:
+        source is some codestate
+
+    Returns:
+        a dictionnary if the kind {<function_name> : <test_number>:int} or None if `source` couldn't be parsed.
     '''
     dico = {}
     finder = L1TestFinder(source=source)
@@ -214,7 +229,9 @@ def find_tests_in_codestate(source:str) -> dict:
         for ex_func in res:
             name = ex_func.get_name().split('(')[0]
             dico[name] = len(ex_func.get_examples())
-    except (ValueError, SyntaxError, SpaceMissingAfterPromptException) as e:
+    except (ValueError, SyntaxError, SpaceMissingAfterPromptException, AttributeError) as e: 
+        # AttributeError levé par thonnycontrib/utils.py
+        # AttributeError: 'Attribute' object has no attribute 'id'
         dico = None
     return dico
 
@@ -223,9 +240,23 @@ def find_tests_in_codestate(source:str) -> dict:
 
 def find_tests_in_codestate_for_functions(codeState:str, functions_tp:list[str]) -> dict:
     '''
-    Returns the number of tests found in codeState, only for functions in functions_tp
+    Returns a dictionnary which associates to each function of `functions_tp` found in `codeState` its number of tests, or None if the function could'nd be found in `codeState`.
 
-    dict has the shape {name_func : int (number of tests) or None (function not found)}
+    Args:
+        codeState : some codeState
+        functions_tp : a list of functions name to consider
+
+    Returns:
+        the dict has the shape {name_func : int (number of tests) or None (function not found)}, or None if codeState not parsable.
+
+    Ex: 
+    codeState1 is not parsable
+    >>> find_tests_in_codestate_for_functions(codeState1, ['foo1', 'foo2', 'foo3'])
+    None
+
+    codeState2 is parsable, foo1 has 3 tests, foo2 is not defined in codeState2, foo3 has 0 test
+    >>> find_tests_in_codestate_for_functions(codeState2, ['foo1', 'foo2', 'foo3'])
+    {'foo1' : 3, 'foo2' : None, 'foo3' : 0}
     '''
     res = {}
     test_number =  find_tests_in_codestate(codeState)
@@ -243,57 +274,89 @@ def find_tests_in_codestate_for_functions(codeState:str, functions_tp:list[str])
 #
 # Elle cherche le codestate le plus récent, et s'il n'est pas analysable, en cherche un analysable dans les précédents. Ce faisant on risque de rater un autre codestate qui aurait pu être analysable, mais tant pis.
 
-def find_tests_for_tp_tpprog_name(name:str, df:pd.DataFrame, tp:str) -> tuple[pd.DataFrame, bool, bool]:
+def find_tests_for_tp_tpprog_name(name:str, df:pd.DataFrame, tp:str, functions_names:dict) -> tuple[pd.DataFrame, bool, bool]:
     """
-    Selects in df rows of tp and Type_TP is TP_Prog, selects for name the most recent parsable codeState and returns :
-    - a DataFrame with columns actor, tp, Tp_Prog, function_name, tests_number (int or None if not present in codestates), index in df of codestate analyzed
-    - a first bool, True if for that student the codeState cannot be analyzed (Python or l1test syntax error)
-    - a second bool, True if for that student no codeState was found
-    """
+    Looks for codeStates related  to 'TP_prog' (df['Type_TP'] == 'TP_prog') for `name` and `tp`, then looks repeatedly for the most recent codeState that can be parsed and contains at least one function of functions_names, then returns:
+
+    - a DataFrame with columns 'actor', 'tp', 'function_name', 'tests_number' (int or None if not present in codestates), 'index' (index in df of the analyzed codeState)
+    - a first bool, True if for that student the codeStates cannot be analyzed (Python or l1test syntax error) or codestates contain no function of functions_names
+    - a second bool, True if for that student and that tp no codeState was found, or only empty codeStates
+
+    codeStates are considered from the most recent to the least recent, so it does not matter if timestamps are not sorted in increaded order.
     
+    Args:
+        name: some actor (ex : 'truc.machin.etu')
+        df: some DataFrame
+        tp: some TP identifiers (ex : 'Tp2')
+        functions_name: a dict which associates to each Tp identifier a list of functions name (ex : functions_names['Tp2] is ['foo1', 'foo2', 'foo3'])
+        
+    Returns:
+        None, False, True: if no codeState was found, or only empty codeStates
+        None, True, False: if no codeState could be parsed, or no codeState contains at least one function of functions_name
+        df, False, False: if some codeState could be parsed and contains 
+    
+    """
     df_name_tp = df[(df['TP'] == tp) & (df['Type_TP'] == 'TP_prog') & ((df['actor'] == name) | (df['binome'] == name))]
     df_codestate_nonempty = df_name_tp[df_name_tp['codeState'] != '']
     if len(df_codestate_nonempty) == 0:
             return None, False, True
     else:
         # look for most recent parsable codeState
-        list_index_timestamps = df_codestate_nonempty['timestamp.$date'].index.tolist()
-        index_of_timestamp_max = df_codestate_nonempty['timestamp.$date'].idxmax()
-        ind_of_timestamp_max_in_list = list_index_timestamps.index(index_of_timestamp_max)
-        index_to_examine = list_index_timestamps[:ind_of_timestamp_max_in_list+1]
-        for i in range(ind_of_timestamp_max_in_list, -1, -1):
-            index = index_to_examine[i]
-            #if df.loc[index]['verb'] == 'Session.Start': # parseable codestate not found
-            #    return None, True, False
-            most_recent_codeState = df_name_tp.loc[index]['codeState']
-            dict_tests = find_tests_in_codestate_for_functions(most_recent_codeState, all_TP_prog_functions_name_by_tp[tp])
+        timestamps = df_codestate_nonempty['timestamp.$date'].copy() # pour être sûre de ne pas modifier le df initial
+        found = False # found codeState with functions not all unfoundable
+        while not timestamps.empty and not found: 
+            index_of_timestamp_max = timestamps.idxmax()
+            most_recent_codeState = df_name_tp.loc[index_of_timestamp_max]['codeState']
+            dict_tests = find_tests_in_codestate_for_functions(most_recent_codeState, functions_names[tp])
             if dict_tests != None: # parseable
                 col_functions = []
                 col_tests_number = []
                 for key, value in dict_tests.items():
                     col_functions.append(key)
                     col_tests_number.append(value)
-                
                 nb_rows = len(dict_tests)
-                if col_tests_number == [None]*nb_rows:
-                     print(f'aucune fonction réalisée par {name}, indice {index}')
-                col_actors = [name] * nb_rows
-                col_tp = [tp] * nb_rows
-                col_index = [index] * nb_rows
-                df_result = pd.DataFrame({'actor' : col_actors, 'tp' : col_tp, 'function_name' : col_functions, \
+                if col_tests_number != [None]*nb_rows: # convenient codestate found
+                    found = True
+                    col_actors = [name] * nb_rows
+                    col_tp = [tp] * nb_rows
+                    col_index = [index_of_timestamp_max] * nb_rows
+                    df_result = pd.DataFrame({'actor' : col_actors, 'tp' : col_tp, 'function_name' : col_functions, \
                                           'tests_number' : col_tests_number, 'index' : col_index })
-                return df_result, False, False                                                      
-        # not parsable
-        return None, True, False
+                else: # codestate no convenient : drop this index and continue
+                    timestamps = timestamps.drop(index=[index_of_timestamp_max])
+            else: # codestate not parsable : drop this index and continue
+                timestamps = timestamps.drop(index=[index_of_timestamp_max])
+        if found:
+            return df_result, False, False
+        else:
+            #print(f"no functions found for {name} and index {index_of_timestamp_max}")
+            return None, True, False
 
 
-def find_tests_for_tp_tpprog(df:pd.DataFrame, tp:str) -> tuple[pd.DataFrame, list[str], list[str]]:
+def find_tests_for_tp_tpprog(df:pd.DataFrame, tp:str, functions_names:dict) -> tuple[pd.DataFrame, list[str], list[str]]:
     """
-    Selects in df rows of tp and Type_TP is TP_Prog, selects for each student the most recent parsable codeState and returns :
-    - a DataFrame with columns actor, tp, Tp_Prog, function_name, test_number (int or None if not present in codestates)
-    - a first list of students for which the codeState cannot be analyzed (Python or l1test syntax error)
-    - a second list of students for which no codeState was found
-    """
+    Looks for codeStates related  to 'TP_prog' (df['Type_TP'] == 'TP_prog') for all students of `tp`, then looks repeatedly for the most recent codeState that can be parsed and contains at least one function of functions_names, then returns:
+
+    - a DataFrame with columns 'actor', 'tp', 'function_name', 'tests_number' (int or None if function not present in codestates), 'index' (index in df of the analyzed codeState)
+    - a first list : actors for which the codeStates for `tp` cannot be analyzed (Python or l1test syntax error) or codestates contain no function of functions_names
+    - a second list, actors for which no codeState was found for `tp`, or only empty codeStates
+
+    codeStates are considered from the most recent to the least recent, so it does not matter if timestamps are not sorted in increaded order.
+    
+    Args:
+        df: some DataFrame
+        tp: some TP identifiers (ex : 'Tp2')
+        functions_name: a dict which associates to each Tp identifier a list of functions name (ex : functions_names['Tp2] is ['foo1', 'foo2', 'foo3'])
+        
+    Returns:
+        a tuple of len 3 :
+        >>> df_tests_tp, cannot_analyze_codestate_students_tp, empty_codestate_students_tp  = find_tests_for_tp_tpprog(df, some_tp, PROG_FUNCTIONS_NAME_BY_TP)
+        - df_tests_tp is a DataFrame of 'columns actor', 'tp', 'function_name', 'tests_number', 'index'
+        - cannot_analyze_codestate_students_tp is the list of actors for which no codeState could be analysed (or no functions found)
+        - empty_codestate_students_tp is the list of actors for which only empty codestate could be found
+        
+        in columns 'test_number': 0 means no tests, None means function not found
+        """
     df_result = pd.DataFrame(columns=['actor', 	'tp', 	'function_name', 	'tests_number', 	'index'])
     empty_codestate_students = []
     cannot_analyze_codestate_students = []
@@ -303,24 +366,77 @@ def find_tests_for_tp_tpprog(df:pd.DataFrame, tp:str) -> tuple[pd.DataFrame, lis
     if '' in all_students_tp:
         all_students_tp.remove('')
     for name in all_students_tp:
-        df_name, cannot_analyze_codestate, empty_codestates = find_tests_for_tp_tpprog_name(name, df, tp)
+        df_name, cannot_analyze_codestate, empty_codestates = find_tests_for_tp_tpprog_name(name, df, tp, functions_names)
         if cannot_analyze_codestate:
             cannot_analyze_codestate_students.append(name)
         if empty_codestates:
             empty_codestate_students.append(name)
         if df_name is not None:
             df_result = pd.concat([df_result, df_name], ignore_index=True)
-    return df_result, cannot_analyze_codestate_students, empty_codestate_students      
+    return df_result, cannot_analyze_codestate_students, empty_codestate_students   
 
 
-df_tests_tp2, cannot_analyze_codestate_students_tp2, empty_codestate_students_tp2  = find_tests_for_tp_tpprog(df, 'Tp2')
+# Exemple d'utilisation
+
+df_tests_tp2, cannot_analyze_codestate_students_tp2, empty_codestate_students_tp2  = find_tests_for_tp_tpprog(df, 'Tp2', PROG_FUNCTIONS_NAME_BY_TP)
 
 print(f'Cannot analyze codestate in Tp2 for : {cannot_analyze_codestate_students_tp2}')
 print(f'only empty codestates in Tp2 for : {empty_codestate_students_tp2}')
 
 df_tests_tp2[0:50]
 
-len(df_tests_tp2)
+len(df_tests_tp2) 
+
+# Not relevant for Tp5 (print week - only 3 functions without print and 2 provided files)
+
+df_tests_tp5, cannot_analyze_codestate_students_tp5, empty_codestate_students_tp5  = find_tests_for_tp_tpprog(df, 'Tp5', PROG_FUNCTIONS_NAME_BY_TP)
+
+df_tests_tp5
+
+print(f'Cannot analyze codestate in Tp5 for : {cannot_analyze_codestate_students_tp5}')
+print(f'only empty codestates in Tp5 for : {empty_codestate_students_tp5}')
+
+
+def find_tests_for_all_tp_tpprog(df:pd.DataFrame, functions_names:dict) -> tuple[pd.DataFrame, dict, dict]:
+    """
+    Looks for codeStates related  to 'TP_prog' (df['Type_TP'] == 'TP_prog') for all students of all tp, then looks repeatedly for the most recent codeState that can be parsed and contains at least one function of functions_names, then returns:
+
+    - a DataFrame with columns 'actor', 'tp', 'function_name', 'tests_number' (int or None if function not present in codestates), 'index' (index in df of the analyzed codeState)
+    - a first dict : keys are tp and values are the list of actors for which the codeStates for `tp` cannot be analyzed (Python or l1test syntax error) or codestates contain no function of functions_names
+    - a second list: keys are tp and values are the list of actors for which no codeState was found for `tp`, or only empty codeStates
+
+    codeStates are considered from the most recent to the least recent, so it does not matter if timestamps are not sorted in increaded order.
+    
+    Args:
+        df: some DataFrame
+        functions_name: a dict which associates to each Tp identifier a list of functions name (ex : functions_names['Tp2] is ['foo1', 'foo2', 'foo3'])
+        
+    Returns:
+        a tuple of len 3 :
+        >>> df_tests_tp, cannot_analyze_codestate_students_tp, empty_codestate_students_tp  = find_tests_for_tp_tpprog(df, PROG_FUNCTIONS_NAME_BY_TP)
+        - df_tests is a DataFrame of 'columns actor', 'tp', 'function_name', 'tests_number', 'index'
+        - cannot_analyze_codestate_students is the dict of {<tp>: <actors>}, actors for which no codeState could be analysed (or no functions found)
+        - empty_codestate_students is the dict of {<tp>: <actors>}, actors for which only empty codestate could be found
+        
+        in columns 'test_number': 0 means no tests, None means function not found   
+    """
+    df_tests = pd.DataFrame(columns=['actor', 	'tp', 	'function_name', 	'tests_number', 	'index'])
+    cannot_analyze_codestate_students = {}
+    empty_codestate_students = {}
+    for tp in functions_names:
+        print(f'TP {tp}')
+        df_tests_tp, cannot_analyze_codestate_students_tp, empty_codestate_students_tp = find_tests_for_tp_tpprog(df, tp, functions_names)
+        df_tests = pd.concat([df_tests, df_tests_tp], ignore_index=True)
+        cannot_analyze_codestate_students[tp] = cannot_analyze_codestate_students_tp
+        empty_codestate_students[tp] = empty_codestate_students_tp
+    return df_tests, cannot_analyze_codestate_students, empty_codestate_students
+
+
+df_tests, cannot_analyze_codestate_students, empty_codestate_students  = find_tests_for_all_tp_tpprog(df, PROG_FUNCTIONS_NAME_BY_TP)
+
+
+
+
 
 
 
